4e08d6468f16102349bcae01a620b201a4e0903f
Satheesh Eppalapelli <150543468+satheesheppalapelli@users.noreply.github.com>
2024-09-23 00:12:23 -0400

    Update MiddleNumbersFinder.java

--- MiddleNumbersFinder.java
+++ MiddleNumbersFinder.java
@@ -2,6 +2,7 @@
 Java Code
 To find the middle number(s) from a list of numbers using Java Streams, 
 you can follow this approach. Depending on whether the number of elements in the list is even or odd:
+you can modify the //code to either return the middle element (for odd-length arrays) or the two middle //elements (for even-length arrays).
 
 1. If the list is odd in size, there will be one middle element.
 2. If the list is even in size, there will be two middle elements.
@@ -33,3 +34,27 @@ public class MiddleNumbersFinder {
         System.out.println("Middle number(s) for even list: " + findMiddleNumbers(numbersEven));
     }
 }
+
+
+// public class MiddleNumbersFinder {
+//   public static void main(String args[]) {
+//     int[] numbers = {15, 9, 8, 4, 2};  // Example with even length
+        
+//         int[] sortedNumbers = Arrays.stream(numbers)
+//                 .sorted()  // Sort the array
+//                 .toArray(); // Collect sorted elements into an array
+        
+//         int len = sortedNumbers.length;
+        
+//         if (len % 2 == 1) {
+//             // If the array length is odd, return the single middle element
+//             int middle = sortedNumbers[len / 2];
+//             System.out.println("Middle number: " + middle);
+//         } else {
+//             // If the array length is even, return the two middle elements
+//             int middle1 = sortedNumbers[(len / 2) - 1];
+//             int middle2 = sortedNumbers[len / 2];
+//             System.out.println("Middle numbers: " + middle1 + " and " + middle2);
+//         }
+//     }
+// }
