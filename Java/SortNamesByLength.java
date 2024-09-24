83bc074325668e23431a583277f376958d3ead7f
Satheesh Eppalapelli <150543468+satheesheppalapelli@users.noreply.github.com>
2024-09-24 09:56:03 -0400

    Update SortNamesByLength.java

--- Java/SortNamesByLength.java
+++ Java/SortNamesByLength.java
@@ -1,7 +1,10 @@
 /*
-Lambdas
+Lambdas:
+
 Lambdas are a concise way to express instances of functional interfaces. 
 Here’s an example where we use a lambda to sort a list of strings by length.
+
+lambda (a, b) -> Integer.compare(a.length(), b.length()) replaces the need for a verbose anonymous inner class and is more readable.
 */
 
 import java.util.Arrays;
